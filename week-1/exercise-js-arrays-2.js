90741012396e9fc80af114b80752121d73bceb8d
sandrabosk <sandraboskovic@hotmail.com>
2020-10-11 21:24:47 +0200

    add exercise 2 solution arrays - part 2

---
+++ week-1/exercise-js-arrays-2.js
@@ -0,0 +1,34 @@
+const fruits = ['apple', 'plum', 'strawberries'];
+
+for (const elemOfArr of fruits) {
+  console.log(elemOfArr);
+}
+
+// apple
+// plum
+// strawberries
+
+console.log('-----------------------------');
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+// apple
+// plum
+// strawberries
+
+console.log('-----------------------------');
+
+// using .forEach() method and show the index of each element
+// next to the element itself.
+
+fruits.forEach(function (elem, index) {
+  console.log(`${elem} ------> ${index}`);
+});
+
+// apple ------> 0
+// plum ------> 1
+// strawberries ------> 2
+
+// fruits.forEach((elem, index) => console.log(`${elem} ------> ${index}`))
