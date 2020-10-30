4595ef95bcc8d0bd7e0e9be8caed8ccb76c0a43c
Marcos <mp2772@gmail.com>
2020-10-29 22:12:48 -0400

    completed viking lab review and added some dom manipulation to display viking war on html

--- week-4/lab-javascript-vikings/src/viking.js
+++ week-4/lab-javascript-vikings/src/viking.js
@@ -3,6 +3,7 @@ class Soldier {
     constructor(health, strength) {
         this.health = health;
         this.strength = strength;
+        this.image = `./images/squire${Math.floor(Math.random() * 4)}.png`;
     }
 
     attack() {
@@ -19,6 +20,7 @@ class Viking extends Soldier {
     constructor(name, health, strength) {
         super(health, strength);
         this.name = name;
+        this.image = `./images/viking${Math.floor(Math.random() * 4)}.png`;
     }
 
     receiveDamage(dmg) {
@@ -38,8 +40,95 @@ class Viking extends Soldier {
 class Saxon extends Soldier {
     constructor(health, strength) {
         super(health, strength);
+        this.image = `./images/saxon${Math.floor(Math.random() * 4)}.png`;
+    }
+
+    receiveDamage(dmg) {
+        this.health -= dmg;
+
+        return this.health > 0
+            ? `A Saxon has received ${dmg} points of damage`
+            : `A Saxon has died in combat`;
     }
 }
 
 // War
-class War {}
+class War {
+    constructor() {
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(viking) {
+        this.vikingArmy.push(viking);
+    }
+
+    addSaxon(saxon) {
+        this.saxonArmy.push(saxon);
+    }
+
+    vikingAttack() {
+        const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+        const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+
+        const theResultOfCombat = this.saxonArmy[randomSaxon]?.receiveDamage(
+            this.vikingArmy[randomViking].attack()
+        );
+
+        if (
+            this.saxonArmy[randomSaxon]?.health -
+                this.vikingArmy[randomViking].attack() <=
+            0
+        ) {
+            this.saxonArmy.splice(randomSaxon, 1);
+        }
+
+        return theResultOfCombat;
+    }
+
+    saxonAttack() {
+        const randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
+        const randomViking = Math.floor(Math.random() * this.vikingArmy.length);
+
+        // optional chaining for object and keys is when you add ? before the . when calling the key. object?.key
+        // link to documentation about optional chaining.
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+        const theResultOfCombat = this.vikingArmy[randomViking]?.receiveDamage(
+            this.saxonArmy[randomSaxon].attack()
+        );
+
+        // let theResultOfCombat;
+
+        // if (this.vikingArmy[randomViking] && this.saxonArmy[randomSaxon]) {
+        //     theResultOfCombat = this.vikingArmy[randomViking].receiveDamage(
+        //         this.saxonArmy[randomSaxon].attack())
+        // };
+
+        if (
+            this.vikingArmy[randomViking]?.health -
+                this.saxonArmy[randomSaxon].attack() <=
+            0
+        ) {
+            this.vikingArmy.splice(randomViking, 1);
+        }
+
+        return theResultOfCombat;
+    }
+
+    showStatus() {
+        // if (this.saxonArmy.length === 0) {
+        //     return "Vikings have won the war of the century!";
+        // } else if (this.vikingArmy.length === 0) {
+        //     return "Saxons have fought for their lives and survived another day...";
+        // } else {
+        //     return "Vikings and Saxons are still in the thick of battle.";
+        // }
+
+        // ternary else if
+        return this.saxonArmy.length === 0
+            ? "Vikings have won the war of the century!"
+            : this.vikingArmy.length === 0
+            ? "Saxons have fought for their lives and survived another day..."
+            : "Vikings and Saxons are still in the thick of battle.";
+    }
+}
